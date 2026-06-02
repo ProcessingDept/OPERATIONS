4d64e22c9687230805f36c914f0abcaef2f66588
ProcessingDept <abafactory@abagold.co.za>
2026-06-02 13:47:14 +0200

    db-config.js

--- assests/js/db-config.js
+++ assests/js/db-config.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'YOUR_PROJECT_URL_HERE';
-const SUPABASE_KEY = 'YOUR_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://yzgwdjbltwkyxjrncvoo.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6Z3dkamJsdHdreXhqcm5jdm9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzOTIyNDUsImV4cCI6MjA5NTk2ODI0NX0.gA4E6_tGqta3-dQ2qAA_w-Bz8A_XNWR_o2UWw7StNOQ';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
